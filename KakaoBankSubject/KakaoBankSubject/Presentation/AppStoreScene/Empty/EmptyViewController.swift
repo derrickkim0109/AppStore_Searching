2aea34dcc3c83fb3d240509fcc64491cb6cd4641
derrickkim0109 <derrickkim0109@naver.com>
2023-03-18 16:17:18 +0900

    feat: 빈 ViewController생성

---
+++ KakaoBankSubject/KakaoBankSubject/Presentation/AppStoreScene/Empty/EmptyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EmptyViewController.swift
+//  KakaoBankSubject
+//
+//  Created by Derrick kim on 2023/03/18.
+//
+
+import UIKit
+
+final class EmptyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
