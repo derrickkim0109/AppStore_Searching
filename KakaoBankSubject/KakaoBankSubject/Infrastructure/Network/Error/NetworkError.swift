0e3298457992d0d1baa00694c35cc74f2f5645ee
derrickkim0109 <derrickkim0109@naver.com>
2023-03-18 11:14:26 +0900

    feat: Network Erro 타입들 생성

---
+++ KakaoBankSubject/KakaoBankSubject/Infrastructure/Network/Error/NetworkError.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkError.swift
+//  KakaoBankSubject
+//
+//  Created by Derrick kim on 2023/03/18.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case error(statusCode: Int, data: Data?)
+    case notConnected
+    case cancelled
+    case generic(Error)
+    case urlGeneration
+}
+
+extension NetworkError {
+    public var isNotFoundError: Bool {
+        return hasStatusCode(404)
+    }
+
+    public func hasStatusCode(
+        _ codeError: Int) -> Bool {
+            switch self {
+            case let .error(code, _):
+                return code == codeError
+            default:
+                return false
+            }
+        }
+}
