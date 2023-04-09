42ed1642f8b386a7e528d4f945f113533d121f4d
derrickkim0109 <derrickkim0109@naver.com>
2023-04-09 19:43:03 +0900

    feat: Task Bag 생성

---
+++ AppStoreSearching/AppStoreSearching/Common/AnyCancellableTask.swift
@@ -0,0 +1,12 @@
+//
+//  AnyCancellableTask.swift
+//  AppStoreSearching
+//
+//  Created by Derrick kim on 2023/03/18.
+//
+
+public protocol AnyCancellableTask {
+    func cancel()
+}
+
+extension Task: AnyCancellableTask {}
