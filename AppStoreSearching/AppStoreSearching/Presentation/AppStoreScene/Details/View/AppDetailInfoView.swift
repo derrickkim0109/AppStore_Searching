c4b91cb19d1394c8098e3bb0ab4ffce07af553cd
derrickkim0109 <derrickkim0109@naver.com>
2023-09-13 00:01:09 +0900

    feat: AppDetailInfoView 생성

---
+++ AppStoreSearching/AppStoreSearching/Presentation/AppStoreScene/Details/View/AppDetailInfoView.swift
@@ -0,0 +1,24 @@
+//
+//  AppDetailInfoView.swift
+//  AppStoreSearching
+//
+//  Created by Derrick kim on 2023/09/07.
+//
+
+import UIKit
+
+final class AppDetailInfoView: BaseView {
+    override func setupDefault() {
+        super.setupDefault()
+    }
+
+    override func addUIComponents() {
+        super.addUIComponents()
+    }
+
+    override func configureLayouts() {
+        super.configureLayouts()
+    }
+
+
+}
